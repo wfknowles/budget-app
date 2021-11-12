27864b5631c0afdfe4fc266e2c54dc8d1c6de0fc
Will Knowles <will@willknowles.com>
2021-11-12 17:55:02 -0600

    heroku fix

--- routes/api.js
+++ routes/api.js
@@ -22,6 +22,7 @@ router.post("/api/transaction/bulk", ({body}, res) => {
 });
 
 router.get("/api/transaction", (req, res) => {
+  
   Transaction.find({}).sort({date: -1})
     .then(dbTransaction => {
       res.json(dbTransaction);
